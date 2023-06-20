18f3bc6f2e527fc0e4dd00fbad9b7d2e961d889f
christol <christol.beresford@hotmail.co.uk>
2023-06-20 18:03:14 +0100

    updated cookie stand details

---
+++ sales app.js
@@ -0,0 +1,68 @@
+"use strict"
+
+console.log ("Cookie shop calculation");
+
+const hours = ["6am, 7am, 8am, 9am, 10am, 11am, 12am, 1pm, 2pm 3pm, 4pm, 5pm, 6pm"];
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }  
+
+const container = doucment.getElementById("container")
+
+const seattle = {
+    storeName: "storeName",
+    minCustPerHour: "minCustPerHour",
+    maxCustPerHour: "maxCustPerHour",
+    avgCookiesPerHour: [],
+    customersEachHour: [],
+    cookiesEachHour: [],
+    totalDailyCookies: 0
+  };
+  Cookiesstore.prototype.calcCustomerEachHour = function() {
+  };
+  
+    const article = document.createElement("article")
+    container.appendChild(h3);
+const ul = doucment.createElement("ul")
+article.appendChild(ul)
+
+for (let i = 0; i <hours.lenght; i++); {
+    const td = document.createElement ("td");
+    td.tectContent = this.cookiesEachHour[i];
+    true.appendChild(td);
+    storeTable
+seattle.render();{
+    this.calcCustomerEachHour.push(randomNum(this.minCustPerHour,this.maxCustPerHour))();
+    this.calcCookiesEachHour();
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//once Seattle is working well, I will invoke the other stores.
+const Lima = {
+    storeName: "Lima",
+    minCustPerHour: 23,
+    maxCustPerHour: 65,
+    avgCookiesPerHour: 6.3,
+    customersEachHour: [33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33],
+    cookiesEachHour: [198, 198, 198, 198, 198, 198, 198, 198, 198, 198, 198, 198, 198, 198],
+    totalDailyCookies: 2772,
+    
+    calcCustomerEachHour: function(){
+        const article = document.createElement("article")
+        container.appendChild(h3);
+    const ul = doucment.createElement("ul")
+    article.appendChild(ul)
+    for (let i = 0; i <hours.lenght; i++);}
+    }
+    Lima.render()}}
