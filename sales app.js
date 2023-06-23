0d756072bdf9f93729a6e66e7d4a134b995a9253
christol <christol.beresford@hotmail.co.uk>
2023-06-23 13:17:14 +0100

    not fully completed

--- sales app.js
+++ sales app.js
@@ -1,68 +1,87 @@
-"use strict"
+"use strict";
 
-console.log ("Cookie shop calculation");
+console.log("Cookie shop calculation");
 
-const hours = ["6am, 7am, 8am, 9am, 10am, 11am, 12am, 1pm, 2pm 3pm, 4pm, 5pm, 6pm"];
 function randomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }  
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-const container = doucment.getElementById("container")
+const hours = [
+  "6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"
+];
 
-const seattle = {
-    storeName: "storeName",
-    minCustPerHour: "minCustPerHour",
-    maxCustPerHour: "maxCustPerHour",
-    avgCookiesPerHour: [],
-    customersEachHour: [],
-    cookiesEachHour: [],
-    totalDailyCookies: 0
-  };
-  Cookiesstore.prototype.calcCustomerEachHour = function() {
-  };
-  
-    const article = document.createElement("article")
-    container.appendChild(h3);
-const ul = doucment.createElement("ul")
-article.appendChild(ul)
+function CookieStore(name, maxCust, avgSale) {
+  this.storeName = name;
+  this.minCustPerHour = 0; // Set default value
+  this.maxCustPerHour = maxCust;
+  this.avgCookiesPerHour = avgSale;
+  this.customersEachHour = [];
+  this.cookiesEachHour = [];
+  this.totalDailyCookies = 0;
+}
 
-for (let i = 0; i <hours.lenght; i++); {
-    const td = document.createElement ("td");
-    td.tectContent = this.cookiesEachHour[i];
-    true.appendChild(td);
-    storeTable
-seattle.render();{
-    this.calcCustomerEachHour.push(randomNum(this.minCustPerHour,this.maxCustPerHour))();
-    this.calcCookiesEachHour();
-}}
+CookieStore.prototype.calcCustomerEachHour = function() {
+  for (let i = 0; i < hours.length; i++) {
+    this.customersEachHour.push(randomNum(this.minCustPerHour, this.maxCustPerHour));
+  }
+};
 
+CookieStore.prototype.calcCookiesEachHour = function() {
+  this.calcCustomerEachHour();
+  for (let i = 0; i < hours.length; i++) {
+    const cookies = Math.round(this.customersEachHour[i] * this.avgCookiesPerHour);
+    this.cookiesEachHour.push(cookies);
+    this.totalDailyCookies += cookies;
+  }
+};
 
+const container = document.getElementById("container");
 
+const seattle = new CookieStore("Seattle", 5, 4);
 
+seattle.calcCookiesEachHour();
 
+const article = document.createElement("article");
+container.appendChild(article);
 
+const h3 = document.createElement("h3");
+h3.textContent = seattle.storeName;
+article.appendChild(h3);
 
+const ul = document.createElement("ul");
+article.appendChild(ul);
 
+for (let i = 0; i < hours.length; i++) {
+  const li = document.createElement("li");
+  li.textContent = hours[i] + ": " + seattle.cookiesEachHour[i] + " cookies";
+  ul.appendChild(li);
+}
 
+const totalLi = document.createElement("li");
+totalLi.textContent = "Total: " + seattle.totalDailyCookies + " cookies";
+ul.appendChild(totalLi);
 
+// Once Seattle is working well, I will invoke the other stores.
+const lima = new CookieStore("Lima", 23, 6.3);
 
+lima.calcCookiesEachHour();
 
+const limaArticle = document.createElement("article");
+container.appendChild(limaArticle);
 
-//once Seattle is working well, I will invoke the other stores.
-const Lima = {
-    storeName: "Lima",
-    minCustPerHour: 23,
-    maxCustPerHour: 65,
-    avgCookiesPerHour: 6.3,
-    customersEachHour: [33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33],
-    cookiesEachHour: [198, 198, 198, 198, 198, 198, 198, 198, 198, 198, 198, 198, 198, 198],
-    totalDailyCookies: 2772,
-    
-    calcCustomerEachHour: function(){
-        const article = document.createElement("article")
-        container.appendChild(h3);
-    const ul = doucment.createElement("ul")
-    article.appendChild(ul)
-    for (let i = 0; i <hours.lenght; i++);}
-    }
-    Lima.render()}}
+const limaH3 = document.createElement("h3");
+limaH3.textContent = lima.storeName;
+limaArticle.appendChild(limaH3);
+
+const limaUl = document.createElement("ul");
+limaArticle.appendChild(limaUl);
+
+for (let i = 0; i < hours.length; i++) {
+  const limaLi = document.createElement("li");
+  limaLi.textContent = hours[i] + ": " + lima.cookiesEachHour[i] + " cookies";
+  limaUl.appendChild(limaLi);
+}
+
+const limaTotalLi = document.createElement("li");
+limaTotalLi.textContent = "Total: " + lima.totalDailyCookies + " cookies";
+limaUl.appendChild(limaTotalLi);
